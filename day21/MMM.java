9e6d7a7a938bf83fdb3a65be771cfa96ac650993
xykzzzz <1105054453@qq.com>
2020-12-16 12:37:58 +0800

    每日一题21

---
+++ day21/MMM.java
@@ -0,0 +1,22 @@
+package 刷题;
+
+public class MMM {
+    public static void main(String[] args) {
+        long n=30; //表示天数
+        long my=meMoney(n);
+        long fu=10*n;
+        System.out.println(fu+" "+my);
+    }
+
+    private static long meMoney(long n) {
+        if(n==1){
+            return (int)Math.pow(2,0);
+        }
+        if(n>1){
+            long res=0;
+            res=res+(long)Math.pow(2,n-1)+meMoney(n-1);
+            return res;
+        }
+        return 0;
+    }
+}
